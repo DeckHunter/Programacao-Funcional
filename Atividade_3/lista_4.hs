a100ed85fab8711b0617e7f8bb8cee9719d53524
DeckHunter <hunterbr100@gmail.com>
2021-06-04 19:04:39 -0300

    segundo commit

--- Atividade_3/lista_4.hs
+++ Atividade_3/lista_4.hs
@@ -3,3 +3,48 @@
 somaImpares :: [Integer] -> Integer
 somaImpares [] = 0
 somaImpares (x:xs) = if (mod x 2) /= 0 then (x + somaImpares xs) else somaImpares xs
+
+--------------- | Max 3 | -------------- 
+
+max3 :: Integer -> Integer -> Integer -> Integer
+max3 x y z
+    | x >= y && x >= z      = x
+    | y >= x && y >= z      = y
+    | z >= x && z >= y      = z
+
+--------------- | Fatorial | --------------
+
+fatorial :: Integer -> Integer
+
+fatorial 0 = 1
+fatorial x = x * fatorial(x-1)
+
+--------------- | Elemento | --------------
+
+elemento :: [Integer] -> Integer -> Integer
+elemento n xs = 0
+
+--------------- | Pertence | --------------
+pertence :: Integer -> [Integer] -> Bool
+pertence n [] = False
+
+pertence n (x:xs)
+    | x == n        = True
+    | x /= n        = pertence n xs
+
+pertence n xs = pertence n xs
+
+--------------- | Total | --------------
+--total :: [Integer] -> Integer
+
+--total = foldr(const (1+)) 0
+
+--------------- | Maior | --------------
+maior :: [Integer] -> Integer
+maior [x] = x
+maior (x : xs) = if (x > (maior xs)) then x else (maior xs)
+
+--------------- | Corpo | --------------
+
+corpo :: [Integer] -> [Integer]
+corpo xs = init xs
